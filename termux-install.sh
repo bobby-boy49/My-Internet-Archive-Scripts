8a600deb6f99439bb6420fd736b2e406aa44dee0
bobby-boy49 <63045905+bobby-boy49@users.noreply.github.com>
2024-01-13 07:27:40 -0500

    Update termux-install.sh

--- termux-install.sh
+++ termux-install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-git clone https://github.com/bobby-boy49/My-Internet-Archive-Scripts.git && cd My-Internet-Archive-Scripts && echo "#!/bin/sh" > mpv && echo "am start --user 0 -a android.intent.action.VIEW -d "$1" -n is.xyz.mpv/.MPVActivity" >> mpv && chmod +x * && rm README.md && mv * /data/data/com.termux/files/usr/bin && cd .. && rm -rf My-Internet-Archive-Scripts
+git clone https://github.com/bobby-boy49/My-Internet-Archive-Scripts.git && cd My-Internet-Archive-Scripts && echo '#!/bin/sh' > mpv && echo 'am start --user 0 -a android.intent.action.VIEW -d "$1" -n is.xyz.mpv/.MPVActivity' >> mpv && chmod +x * && rm README.md && mv * /data/data/com.termux/files/usr/bin && cd .. && rm -rf My-Internet-Archive-Scripts
