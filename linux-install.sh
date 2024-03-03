febb223f32559023215382f620513eb288e9829b
bobby-boy49 <63045905+bobby-boy49@users.noreply.github.com>
2024-03-02 22:56:47 -0500

    Update linux-install.sh

--- linux-install.sh
+++ linux-install.sh
@@ -2,6 +2,8 @@
 
 # This will install the scripts in the directory this file is in!
 
+archiveus=$(rm -f archiveus* && echo "archiveus and archiveusa are no longer working because of javascript. :/ *SCRIPTS REMOVED!*")
+
 git clone https://github.com/bobby-boy49/My-Internet-Archive-Scripts.git
 
 cd My-Internet-Archive-Scripts
@@ -12,9 +14,12 @@ chmod +x *
 
 cd ..
 
-if [ -f $(pwd)/archiveus* ]
+if [ -f $(pwd)/archiveus ]
 then
-	rm -f archiveus* && echo "archiveus and archiveusa are no longer working because of javascript. :/ *SCRIPTS REMOVED!*"
+	$archiveus
+elif [ -f $(pwd)/archiveusa ]
+then
+	$archiveus
 else
 	echo "archiveus and archiveusa does not exist ignoring..."
 fi
