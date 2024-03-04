e41fc0796a1f8a8ae33404372e7f19b3316cf607
bobby-boy49 <63045905+bobby-boy49@users.noreply.github.com>
2024-03-03 22:43:49 -0500

    Update termux-install.sh

--- termux-install.sh
+++ termux-install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git clone https://github.com/bobby-boy49/My-Internet-Archive-Scripts.git && cd My-Internet-Archive-Scripts && echo '#!/bin/sh' > mpv && echo 'am start --user 0 -a android.intent.action.VIEW -d "$1" -n is.xyz.mpv/.MPVActivity' >> mpv && chmod +x * && rm -f README.md termux-install.sh linux-install.sh archiveus archiveusa && mv * /data/data/com.termux/files/usr/bin && cd .. && rm -rf My-Internet-Archive-Scripts && rm termux-install.sh && echo '#!/bin/sh' > termux-install.sh && echo 'curl -s https://raw.githubusercontent.com/bobby-boy49/My-Internet-Archive-Scripts/main/termux-install.sh | sh' >> termux-install.sh && chmod +x termux-install.sh
+
 if cd /data/data/com.termux/files/usr/bin && find -type f | grep -q 'archiveus' || cd /data/data/com.termux/files/usr/bin && find -type f | grep -q 'archiveusa'; then
 	rm -f archiveus*
 	cd
@@ -8,5 +10,3 @@ else
 	cd
 	echo "archiveus and archiveusa does not exist ignoring..."
 fi
-
-git clone https://github.com/bobby-boy49/My-Internet-Archive-Scripts.git && cd My-Internet-Archive-Scripts && echo '#!/bin/sh' > mpv && echo 'am start --user 0 -a android.intent.action.VIEW -d "$1" -n is.xyz.mpv/.MPVActivity' >> mpv && chmod +x * && rm -f README.md termux-install.sh linux-install.sh archiveus archiveusa && mv * /data/data/com.termux/files/usr/bin && cd .. && rm -rf My-Internet-Archive-Scripts && rm termux-install.sh && echo '#!/bin/sh' > termux-install.sh && echo 'curl -s https://raw.githubusercontent.com/bobby-boy49/My-Internet-Archive-Scripts/main/termux-install.sh | sh' >> termux-install.sh && chmod +x termux-install.sh
